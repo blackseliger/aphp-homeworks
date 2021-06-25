0f45dbfe7b6c3fb1fcaaeec4e3d92e9316cf8aff
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2021-06-26 01:36:20 +0300

    add request

--- 004-db/workDB/main.php
+++ 004-db/workDB/main.php
@@ -1,1 +1,60 @@
 <?php
+
+
+//CREATE TABLE shop (id INT AUTO_INCREMENT PRIMARY KEY,
+//    name varchar(256) NOT NULL,
+//    address varchar(256) NOT NULL
+//);
+//
+//CREATE TABLE product ( id INT AUTO_INCREMENT PRIMARY KEY,
+//    name varchar(256) NOT NULL ,
+//    price int(10) NOT NULL ,
+//    count int (256) NOT NULL
+// );
+//
+//    CREATE TABLE order1(id INT AUTO_INCREMENT PRIMARY KEY ,
+//                       created_at datetime NOT NULL
+//    );
+//
+//CREATE TABLE order_product (id INT AUTO_INCREMENT PRIMARY KEY ,
+//    products varchar(256) NOT NULL
+//);
+//
+//CREATE TABLE client (id INT AUTO_INCREMENT PRIMARY KEY ,
+//    phone int(10) NOT NULL ,
+//    name varchar(256) NOT NULL
+//);
+//
+//
+//INSERT INTO shop(name, address) values ('ivan', '123456');
+//INSERT INTO shop(name, address) values ('gleb', '1111123456');
+//INSERT INTO shop(name, address) values ('andrey', '2112123456');
+//INSERT INTO shop(name, address) values ('sergey', '001256');
+//INSERT INTO shop(name, address) values ('nikita', '11116');
+//
+//
+//INSERT INTO product(name, price, count) values ('coca-cola', 100, 2);
+//INSERT INTO product(name, price, count) values ('pepsi', 100, 3);
+//INSERT INTO product(name, price, count) values ('fanta', 120, 4);
+//INSERT INTO product(name, price, count) values ('adrenaline', 90, 5);
+//INSERT INTO product(name, price, count) values ('meet', 11, 10);
+//
+//INSERT INTO order1(created_at) values ('123456');
+//INSERT INTO order1(created_at) values ('1111123456');
+//INSERT INTO order1(created_at) values ('2112123456');
+//INSERT INTO order1(created_at) values ('001256');
+//INSERT INTO order1(created_at) values ('11116');
+//
+//INSERT INTO order_product(products) values ('fanta');
+//INSERT INTO order_product(products) values ('pepsi');
+//INSERT INTO order_product(products) values ('milk');
+//INSERT INTO order_product(products) values ('ice cream');
+//INSERT INTO order_product(products) values ('tea');
+//
+//INSERT INTO client(phone, name) values (4000-450-20, 'artyom');
+//INSERT INTO client(phone, name) values (4000-500-10, 'valera');
+//INSERT INTO client(phone, name) values (4010-510-50, 'igor');
+//INSERT INTO client(phone, name) values (5020-202-10, 'maksim');
+//INSERT INTO client(phone, name) values (1020-202-11, 'gleb');
+
+
